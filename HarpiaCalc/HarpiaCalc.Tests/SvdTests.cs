cebaa7d95103ed8ecc7cbc241b7c42ff583980fc
Vytautas Butkus <vytautas.butkus@lightcon.com>
2024-06-03 10:43:08 +0300

    Added HarpiaCalc.Tests for unit testing

---
+++ HarpiaCalc/HarpiaCalc.Tests/SvdTests.cs
@@ -0,0 +1,11 @@
+namespace HarpiaCalc.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CalculateSvdTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
